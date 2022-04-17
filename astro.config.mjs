9f78a298e3cc380daf64d1e2d6be4fc6d6c2865c
Alex Trost <19617280+a-trost@users.noreply.github.com>
2022-04-17 18:52:50 -0400

    Remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 import turbolinks from "@astrojs/turbolinks";
 
-// import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://happyclicks.dev",
